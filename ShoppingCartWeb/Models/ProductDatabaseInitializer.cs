594fe5a5d5d51e813720b93848d858d9f56df51a
AndreeaDraghici <draghici108@gmail.com>
2023-03-03 21:57:38 +0200

    chore: small changes into app_data directory

--- ShoppingCartWeb/Models/ProductDatabaseInitializer.cs
+++ ShoppingCartWeb/Models/ProductDatabaseInitializer.cs
@@ -57,7 +57,7 @@ namespace ShoppingCartWeb.Models
                     ProductName = "Convertible Car",
                     Description = "This convertible car is fast! The engine is powered by a neutrino based battery (not included)." +
                                   "Power it up and let it go!",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 22.50,
                     CategoryID = 1
                },
@@ -66,7 +66,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 2,
                     ProductName = "Old-time Car",
                     Description = "There's nothing old about this toy car, except it's looks. Compatible with other old toy cars.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 15.95,
                      CategoryID = 1
                },
@@ -75,7 +75,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 3,
                     ProductName = "Fast Car",
                     Description = "Yes this car is fast, but it also floats in water.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 32.99,
                     CategoryID = 1
                 },
@@ -84,7 +84,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 4,
                     ProductName = "Super Fast Car",
                     Description = "Use this super fast car to entertain guests. Lights and doors work!",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 8.95,
                     CategoryID = 1
                 },
@@ -94,7 +94,7 @@ namespace ShoppingCartWeb.Models
                     ProductName = "Old Style Racer",
                     Description = "This old style racer can fly (with user assistance). Gravity controls flight duration." +
                                   "No batteries required.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 34.95,
                     CategoryID = 1
                 },
@@ -103,7 +103,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 6,
                     ProductName = "Ace Plane",
                     Description = "Authentic airplane toy. Features realistic color and details.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 95.00,
                     CategoryID = 2
                 },
@@ -112,7 +112,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 7,
                     ProductName = "Glider",
                     Description = "This fun glider is made from real balsa wood. Some assembly required.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 4.95,
                     CategoryID = 2
                 },
@@ -121,7 +121,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 8,
                     ProductName = "Paper Plane",
                     Description = "This paper plane is like no other paper plane. Some folding required.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 2.95,
                     CategoryID = 2
                 },
@@ -130,7 +130,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 9,
                     ProductName = "Propeller Plane",
                     Description = "Rubber band powered plane features two wheels.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 32.95,
                     CategoryID = 2
                 },
@@ -139,7 +139,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 10,
                     ProductName = "Early Truck",
                     Description = "This toy truck has a real gas powered engine. Requires regular tune ups.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 15.00,
                     CategoryID = 3
                 },
@@ -148,7 +148,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 11,
                     ProductName = "Fire Truck",
                     Description = "You will have endless fun with this one quarter sized fire truck.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 26.00,
                     CategoryID = 3
                 },
@@ -157,7 +157,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 12,
                     ProductName = "Big Truck",
                     Description = "This fun toy truck can be used to tow other trucks that are not as big.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 29.00,
                     CategoryID = 3
                 },
@@ -167,7 +167,7 @@ namespace ShoppingCartWeb.Models
                     ProductName = "Big Ship",
                     Description = "Is it a boat or a ship. Let this floating vehicle decide by using its " +
                                   "artifically intelligent computer brain!",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 95.00,
                     CategoryID = 4
                 },
@@ -177,7 +177,7 @@ namespace ShoppingCartWeb.Models
                     ProductName = "Paper Boat",
                     Description = "Floating fun for all! This toy boat can be assembled in seconds. Floats for minutes!" +
                                   "Some folding required.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 4.95,
                     CategoryID = 4
                 },
@@ -186,7 +186,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 15,
                     ProductName = "Sail Boat",
                     Description = "Put this fun toy sail boat in the water and let it go!",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 42.95,
                     CategoryID = 4
                 },
@@ -195,7 +195,7 @@ namespace ShoppingCartWeb.Models
                     ProductID = 16,
                     ProductName = "Rocket",
                     Description = "This fun rocket will travel up to a height of 200 feet.",
-                    ImagePath="C:\\Users\\user\\source\\repos\\ShoppingCartWeb\\ShoppingCartWeb\\Images\\icon.png",
+                    ImagePath="car.png",
                     UnitPrice = 122.95,
                     CategoryID = 5
                 }
